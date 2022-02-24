689fb42a11263a380dddd868c661be1b138627f6
Alex Wade <alexjohnwade@gmail.com>
2022-02-24 16:45:47 +1100

    got it all working style needs some work

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'lists', to: 'lists#index', as: :lists
+  # get 'lists/new', to: 'lists#new', as: :new_list
+  # post 'lists', to: 'lists#create'
+  # get 'lists/:id', to: 'lists#show', as: :list
+  # get 'lists/:list_id/bookmarks/new', to: 'bookmarks#new'
+  # post 'lists/:list_id/bookmarks', to: 'bookmarks#create'
+  # delete 'bookmarks/:list_id', to: 'lists#destroy'
+
+  resources :lists, only: [:index, :new, :show, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
